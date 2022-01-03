8b828baf1955079ec8fd4aac17ba7f0a8da85ac6
mihran <mihran.eprem@gmail.com>
2022-01-02 23:55:36 -0300

    Initial commit

---
+++ src/main/java/com/salada/salada/SaladaApplication.java
@@ -0,0 +1,13 @@
+package com.salada.salada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaladaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaladaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salada/salada/SaladaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salada.salada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
