e6b7c50dca19c1549fd0afa370b289d7b38aa528
mihran <mihran.eprem@gmail.com>
2022-01-09 00:46:12 -0300

    adding inv

--- src/main/java/com/salada/salada/services/TestService.java
+++ src/main/java/com/salada/salada/services/TestService.java
@@ -1,5 +1,6 @@
 package com.salada.salada.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/service")
 public class TestService {
 
+    @Value("#{name}")
+    private  String name ;
     @GetMapping("/")
     public String get(){
-        return "hi" ;
+        return "hi " + name ;
 
     }
 
